60239036c66ab187c84229c87383eec034ddb856
oAndin <00andherson@gmail.com>
2023-06-15 10:30:35 -0300

    Upando os arquivos

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Hello World!");
+let resposta = document.querySelector(".resposta");
+btn.onclick = function () {
+    let altura = altura.value;
+    let peso = peso.value;
+    let calculo = (altura * altura) / peso ; 
+    resposta.innerText = `Seu IMC é: ${calculo.toFixed(1)}`;
+
+    if (calculo <= 18.4) {resposta.innerText += ` Abaixo do peso`
+    return;
+}
+    if (calculo <= 24.9) {resposta.innerText += ` Peso ideal`
+    return;
+}
+    if (calculo <= 29.9) {resposta.innerText += ` Excesso de peso`
+    return;
+}
+    if (calculo <= 34.9) {resposta.innerText += ` Obesidade I`
+    return;
+}
+    if (calculo <= 39.9) {resposta.innerText += ` Obesidade II`
+    return;
+}
+    else {resposta.innerText += ` Obesidade III`};
+};
